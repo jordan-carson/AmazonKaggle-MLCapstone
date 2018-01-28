3a57574907485ba142c0e48a994ba9f5a6d4ac7a
jordan-carson <jordanlouiscarson@gmail.com>
2018-01-28 14:42:51 -0500

    [ADD] added plot_pictures definition

--- src/utillities/utility.py
+++ src/utillities/utility.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import os
+import random
+from PIL import Image
+import matplotlib.pyplot as plt
+
 # this contains image_name and tags for training set
 
 class FileNotFoundError:
@@ -20,5 +24,20 @@ def read_file(filepath, filename, filetype):
                         print(err)
 
 
-df_labels = read_file('/Users/jordancarson/PyCharmProjects/AmazonKaggle-MLCapstone/resources', 'train_v2.csv', 'csv')
+# df_labels = read_file('/Users/jordancarson/PyCharmProjects/AmazonKaggle-MLCapstone/resources', 'train_v2.csv', 'csv')
 
+def plot_pictures(df_train, label, TRAIN_PATH):
+
+    images = df_train[df_train[label] == 1].image_name.values
+
+    fig , ax = plt.subplots(nrows=3, ncols=3, figsize=(8,8))
+    ax = ax.flatten()
+
+    for i in range(0,9):
+        f = random.choice(images)
+        img = Image.open(os.path.join(TRAIN_PATH, f + '.jpg'))
+        ax[i].imshow(img)
+        ax[i].set_xticks([])
+        ax[i].set_yticks([])
+        ax[i].set_title("{}s h:{}s w:{}s".format(f, img.height, img.width))
+    plt.tight_layout()
