aaad41e2af9390148da3116a715399cb2313f4ab
jordan-carson <jordanlouiscarson@gmail.com>
2018-02-11 10:59:26 -0500

    [ADD] image processing with OpenCV

--- src/utillities/common/preprocess.py
+++ src/utillities/common/preprocess.py
@@ -1,5 +1,8 @@
 import os
 import numpy as np, pandas as pd
+from keras import backend as K
+from keras.engine.topology import Layer
+from PIL import Image
 
 
 class Preprocessor:
@@ -21,5 +24,57 @@ class Preprocessor:
         self.img_resize = img_resize
         self.validation_split = validation_split
 
-    def load_labels(self):
-        pd.read_csv(self.train_csv, sep=',')
+    def process_images(self, array):
+        """
+        module to process the images to greyscale and output an np.array
+        :return: np.array
+        """
+        import cv2  
+        
+        return cv2.resize(array)
+
+
+class BuildModel:
+
+    def __init__(self, dropout):
+        self.dropout = dropout
+
+
+class MyDense(Layer):
+
+    def __init__(self, output_dim, **kwargs):
+        self.units = output_dim
+        super(MyDense, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight(name='kernel', shape=(input_shape[1], self.output_dim),
+                                      initializer='uniform', trainable=True)
+        super(MyDense, self).build(input_shape)
+
+    def call(self, x, **kwargs):
+        """This is where the layer's logic lives.
+
+        # Arguments
+            inputs: Input tensor, or list/tuple of input tensors.
+            **kwargs: Additional keyword arguments.
+
+        # Returns
+            A tensor or list/tuple of tensors.
+        """
+        y = K.dot(x, self.kernel)
+        return y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
