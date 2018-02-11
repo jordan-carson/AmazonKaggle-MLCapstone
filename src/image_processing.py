48e296dd42137ac958c4970478c105eafa54ce37
jordan-carson <jordanlouiscarson@gmail.com>
2018-02-11 10:59:33 -0500

    [ADD] image processing with OpenCV

--- src/image_processing.py
+++ src/image_processing.py
@@ -2,15 +2,13 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
 import gc
+import logging
+from src.utillities.common.CNN import optimise_f2_thresholds, n_crossvalidation
 
 import numpy as np
 from sklearn.metrics import fbeta_score
 
-import keras as k
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+
 
 import cv2
 from tqdm import tqdm
@@ -72,6 +70,7 @@ label_map = {'agriculture': 14,
              'selective_logging': 12,
              'slash_burn': 8,
              'water': 15}
+
 for f, tags in tqdm(df_train.values[:18000], miniters=1000):
     img = cv2.imread(os.path.join(MAIN, TRAIN_PATH) + '{}.jpg'.format(f))
     targets = np.zeros(17)
@@ -79,7 +78,6 @@ for f, tags in tqdm(df_train.values[:18000], miniters=1000):
         targets[label_map[t]] = 1
     x_train.append(cv2.resize(img, (32, 32)))
     y_train.append(targets)
-
 
 # for f, tags in tqdm(df_test.values, miniters=1000):
 #     img = cv2.imread(os.path.join(MAIN, TEST_PATH) + '{}.jpg'.format(f))
@@ -103,47 +101,27 @@ def get_optimal_threshhold(true_label, prediction, iterations = 100):
             temp_value = i / float(iterations)
             temp_threshhold[t] = temp_value
             temp_fbeta = fbeta(true_label, prediction > temp_threshhold)
-            if  temp_fbeta > best_fbeta:
+            if temp_fbeta > best_fbeta:
                 best_fbeta = temp_fbeta
                 best_threshhold[t] = temp_value
+
 
 def fbeta(true_label, prediction):
    return fbeta_score(true_label, prediction, beta=2, average='samples')
 
 
 
-def optimise_f2_thresholds(y, p, verbose=True, resolution=100):
-  def mf(x):
-    p2 = np.zeros_like(p)
-    for i in range(17):
-      p2[:, i] = (p[:, i] > x[i]).astype(np.int)
-    score = fbeta_score(y, p2, beta=2, average='samples')
-    return score
 
-  x = [0.2]*17
-  for i in range(17):
-    best_i2 = 0
-    best_score = 0
-    for i2 in range(resolution):
-      i2 /= resolution
-      x[i] = i2
-      score = mf(x)
-      if score > best_score:
-        best_i2 = i2
-        best_score = score
-    x[i] = best_i2
-    if verbose:
-      print(i, best_i2, best_score)
-
-  return x
-
-prediction = np.random.rand(50000,17)
-true_label = np.random.rand(50000,17) > 0.5
+prediction = np.random.rand(50000, 17)
+true_label = np.random.rand(50000, 17) > 0.5
 
 start = time.time()
 t1 = get_optimal_threshhold(true_label, prediction)
 print(time.time() - start)
-
 start = time.time()
+
 t2 = optimise_f2_thresholds(true_label, prediction)
 print(time.time() - start)
+
+
+
