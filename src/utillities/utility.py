14627d6bd7f4d9016b1ddee65e226a9102cd8c1d
jordan-carson <jordanlouiscarson@gmail.com>
2018-02-12 21:37:44 -0500

    adding init_logger for logging from utility functions

--- src/utillities/utility.py
+++ src/utillities/utility.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import os
+import socket
+import logging
+import logging.handlers
+import sys
+import datetime
+
 import random
 from PIL import Image
 import matplotlib.pyplot as plt
-import logging
 
 # df_labels = read_file('/Users/jordancarson/PyCharmProjects/AmazonKaggle-MLCapstone/resources', 'train_v2.csv', 'csv')
 
@@ -24,15 +29,46 @@ def plot_pictures(label, df_train, TRAIN_PATH):
     plt.tight_layout()
 
 
-def init_logger(filename, directory):
-    if not os.path.exists(directory):
-        os.mkdir(directory)
+def init_logger(log_dir, process_name, loglevel_file=20, loglevel_stdout=40):
+    logging.getLogger().setLevel(logging.NOTSET) #0
+    logging.getLogger().handlers = []
+    timestamp = datetime.datetime.now().strftime('%Y%m%d')
+    logfile_name = process_name + '_' + str(socket.gethostname()) + '_' + str(os.getenv('Username')) \
+                   + '_' + timestamp + '.log'
+    logfile_path = os.path.join(log_dir, logfile_name)
 
-    logging.Formatter(datefmt='%Y%m%d %H:%M:%S')
-    # logging.basicConfig(filename=filename, **kwargs)
-    fileh = logging.FileHandler(filename, 'a')
+    # create directory if it doesn't already exist
+    try:
+        os.makedirs(log_dir)
+    except:
+        pass
+
+    # delete previous version of this logfile if it exists
+
+    try:
+        os.remove(logfile_name)
+    except:
+        pass
+
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fileh.setFormatter(formatter)
+
+    # file handler
+    file_h = logging.handlers.RotatingFileHandler(logfile_path, mode='w', maxBytes=50*1024*1024,
+                                                  backupCount=2)
+    file_h.setLevel(loglevel_file)  # 20 = info
+    file_h.setFormatter(formatter)
+
+    # stdout handler
+    stdout_h = logging.StreamHandler(stream=sys.stdout)
+    stdout_h.setLevel(loglevel_stdout)  # 40 = error
+    stdout_h.setFormatter(formatter)
+    logging.getLogger().addHandler(stdout_h)
+
+    logging.info('Logging initialised. PID ' + str(os.getpid()))
+
+
+
+
 
 
 
