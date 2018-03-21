580f69814b5db49becb696cb1a83d354502ab37e
jordan-carson <jordanlouiscarson@gmail.com>
2018-03-20 23:33:12 -0400

    [UPD] adding functions for custom builds vgg16, 19, and sequential model

--- src/image_processing.py
+++ src/image_processing.py
@@ -119,6 +119,8 @@ true_label = np.random.rand(18000, 17) > 0.5
 """
     module takes 388.4562318325043 to run or 6.5 mins
 """
+
+
 def Amazon_Sequential_Custom_Build(input_shape = (128, 128), weight_path=None):
     """
 
@@ -155,24 +157,77 @@ def Amazon_Sequential_Custom_Build(input_shape = (128, 128), weight_path=None):
     custom_model.add(Dense(17, activation='sigmoid'))
     if (weight_path!=None):
         if os.path.isfile(weight_path):
-            model.load_weights(weight_path)
+            custom_model.load_weights(weight_path)
     return custom_model
+
 
 def Amazon_Model_VGG19(input_shape=(128, 128, 3), weight_path=None):
     from keras.applications.vgg19 import VGG19
 
     base_model = VGG19(include_top=False, weights='imagenet', input_shape=input_shape)
 
-    custom_vgg = Sequential()
-    custom_vgg.add(BatchNormalization(input_shape=input_shape))
-    custom_vgg.add(base_model)
-    custom_vgg.add(Flatten())
+    model = Sequential()
+    model.add(BatchNormalization(input_shape=input_shape))
+    model.add(base_model)
+    model.add(Flatten())
+    model.add(Dense(17, activation='sigmoid'))
+    if (weight_path!=None):
+        if os.path.isfile(weight_path):
+            model.load_weights(weight_path)
+    return model
 
 
+def Amazon_Model_VGG16(input_shape=(128, 128, 3), weight_path=None):
+    from keras.applications.vgg16 import VGG16
 
+    base_model = VGG16(include_top=False, weights='imagenet', input_shape=input_shape)
 
+    model = Sequential()
+    model.add(BatchNormalization(input_shape=input_shape))
+    model.add(base_model)
+    model.add(Flatten())
+    model.add(Dense(17, activation='sigmoid'))
+    if (weight_path!=None):
+        if os.path.isfile(weight_path):
+            model.load_weights(weight_path)
+    return model
 
+def KFold_Train(x_train, y_train, n_folds=3, batch_size=128):
+    model = Amazon_Sequential_Custom_Build()
 
+    kfold = KFold(len(y_train), n_folds=n_folds, shuffle=False, random_state=1)
+    num_fold = 0
+
+    for train_index, test_index in kf:
+
+        X_train = x_train[train_index]
+        Y_train = y_train[train_index]
+        X_valid = x_train[test_index]
+        Y_valid = y_train[test_index]
+
+        num_fold += 1
+        print('Start KFold number {} from {}'.format(num_fold, n_folds))
+        print('Split train: ', len(X_train), len(Y_train))
+        print('Split valid: ', len(X_valid), len(Y_valid))
+        weight_path = os.path.join('', 'weights_kfold_' + str(num_fold) + '.h5')
+
+        if os.path.isfile(weight_path):
+            model.load_weights(weight_path)
+
+        epochs_arr = [60, 15, 15]
+        learn_rates = [0.001, 0.0001, 0.00001]
+
+        for learn_rate, epochs in zip(learn_rates, epochs_arr):
+            optimizer = optimizers.Adam(lr=learn_rate)
+            model.compile(loss='binary_crossentropy', metrics=['accuracy'], optimizer=optimizer)
+            callbacks = [EarlyStopping(monitor='val_loss', patience=2, verbose=0),
+                         ModelCheckpoint(weight_path, monitor='val_loss', save_best_only=True, verbose=0)]
+
+            model.fit(x=X_train, y=Y_train, validation_data=(X_valid, Y_valid),
+                      batch_size=batch_size, verbose=2, epochs=epochs, callbacks=callbacks, shuffle=True)
+
+        p_valid = model.predict(X_valid, batch_size=batch_size, verbose=2)
+        print(fbeta_score(Y_valid, np.array(p_valid) > 0.18, beta=2, average='samples'))
 
 
 
